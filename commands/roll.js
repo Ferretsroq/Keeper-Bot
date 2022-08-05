8e724a5b3560c19ca9c05058ad544db8d50f3a0d
Ferretsroq <Ferretsroq@users.noreply.github.com>
2022-08-05 10:07:54 -0400

    Adding embed to roll message

--- commands/roll.js
+++ commands/roll.js
@@ -1,5 +1,5 @@
 const {SlashCommandBuilder} = require('@discordjs/builders');
-const {ActionRowBuilder, ButtonBuilder, ButtonStyle} = require('discord.js');
+const {ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder} = require('discord.js');
 const Character = require('../characters.js');
 
 module.exports = 
@@ -7,15 +7,21 @@ module.exports =
 	data: new SlashCommandBuilder()
 	.setName('roll')
 	.setDescription('Roll 2d6+stat!')
+	.addStringOption(option =>
+		option.setName('description')
+		.setRequired(true)
+		.setDescription('Description of your roll'))
 	.addIntegerOption(option =>
 		option.setName('bonus')
 		.setRequired(false)
 		.setDescription('Bonus to add to your roll')),
 	character: null,
 	bonus: 0,
+	description: '',
 	async execute(interaction, characters, players)
 	{
 		this.bonus = interaction.options.getInteger('bonus');
+		this.description = interaction.options.getString('description');
 		if(this.bonus === null)
 		{
 			this.bonus = 0;
@@ -54,6 +60,8 @@ module.exports =
 		{
 			result = 'Mixed Success'
 		}
-		await interaction.update({content: `**${result}**\n${die1}+${die2}+${statBonus}+${this.bonus} = ${total}\nRolled with ${stat}`, components: []})
+		let embed = new EmbedBuilder().setTitle(`Roll for ${this.character.alias} — ${this.description}`).setDescription(`You rolled **${total}**\n**${result}**\n((${die1}+${die2} + ${statBonus} for stat ${stat} + ${this.bonus} for ongoing/forward)`);
+		await interaction.update({embeds: [embed], components: []});
+		//await interaction.update({content: `**${result}**\n${die1}+${die2}+${statBonus}+${this.bonus} = ${total}\nRolled with ${stat}`, components: []})
 	},
 };
